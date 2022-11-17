b17c51604c1d69bfc28ff07191847d0328d81e3a
GLADYS GARCIA <48798967+GladysMaria@users.noreply.github.com>
2022-11-17 18:14:00 -0300

    catalog/models/book issue
    Don't work --> return reverse('book-detail', args=[str(self.id)])
    Works -->         return reverse('catalog:book-detail', args=[str(self.id)])
    ( django --version 4.1.3)

--- catalog/models.py
+++ catalog/models.py
@@ -54,7 +54,7 @@ class Book(models.Model):
 
     def get_absolute_url(self):
         """Returns the url to access a particular book instance."""
-        return reverse('book-detail', args=[str(self.id)])
+        return reverse('catalog:book-detail', args=[str(self.id)])
 
     def __str__(self):
         """String for representing the Model object."""
